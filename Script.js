1dbdfcf9ce7f725c57d812924803bdddc36824d7
Shabhaizafar <zafarshabhai03@gmail.com>
2025-05-26 15:45:36 +0530

    Templates engine

--- Script.js
+++ Script.js
@@ -1,6 +1,7 @@
 const  express = require("express");
 
 const app = express();
+app.set("view engine",'ejs');
 
 // Middleware 
 // app.use(function(request,response,next){
@@ -10,17 +11,37 @@ const app = express();
 
 // app.get(route path,function);
 
-app.get("/",function(request,response){
-    console.log(response);
-    response.send("Hello Everyone,Welcome");
+// app.get("/",function(request,response){
+//     console.log(response);
+//     response.send("Hello Everyone,Welcome");
+// });
+
+// app.get('/profile',function(request,response){
+//     response.send('<h1>Hello Everyone Welcome to Profile</h1>');
+// });
+
+app.get('/profile',function(request,response){
+    response.render('profile');
 });
 
-
-app.get("/contact",function(request,response){
-    response.send("Contact Here");
+app.get('/contact/:uname',function(request,response){
+    console.log(request.params);
+    response.render('contact',request.params);
 });
-app.get("/contact/:username",function(request,response){
-    response.send(`Profile Page : ${request.params.username}`);
+app.get('/',function(request,response){
+    response.render('home');
 });
+// app.get('/profile/:username',function(request,response){
+//     response.send(`<h1>Hello Everyone Welcome!!${request.params.username}</h1>`);
+// });
+// app.get("/contact",function(request,response){
+//     response.send("Contact Here");
+// });
+// app.get("/contact/:username",function(request,response){
+//     response.send(`Profile Page : ${request.params.username}`);
+// });
 
 app.listen(3000);
+
+
+// https://themewagon.github.io/game-warrior/review.html
