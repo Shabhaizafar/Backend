052e2252fc8a68b06a1fdae84ae08522a3e23aea
Shabhaizafar <zafarshabhai03@gmail.com>
2025-07-03 15:25:44 +0530

    Login Page

--- registeradd/routes/index.js
+++ registeradd/routes/index.js
@@ -3,22 +3,50 @@ var router = express.Router();
 var student = require('./users');
 
 /* GET home page. */
+// registerpage
+// router.get('/', function(req, res) {
+//   res.render('index');
+// });
+
+
+
+// register submit
+// router.post('/submit', async function(req, res) {
+//   var obj = req.body;
+//      var s = await student.create({
+//       firstname : obj.firstname,
+//       lastname : obj.lastname
+//     });
+//   res.send("Register Successfully !!!");
+// });
+
+// router.get('/check', async function(req, res) {
+//      var s = await student.find();
+//   res.send(s);
+// });
+
+
+
+// login page
 router.get('/', function(req, res) {
-  res.render('index');
+  res.render('login');
 });
 
-router.post('/submit', async function(req, res) {
+// login page : signin
+router.post('/signin', async function(req, res) {
   var obj = req.body;
-     var s = await student.create({
-      firstname : obj.firstname,
-      lastname : obj.lastname
-    });
-  res.send("Register Successfully !!!");
+
+  var sall = await student.find({
+    firstname : obj.firstname,
+    lastname : obj.lastname
+  });
+  console.log(sall[0]?.firstname);
+  if(!sall[0]?.firstname){
+    res.render('index');
+  }else{
+    res.send("Login SuccessFully"); 
+  }
 });
 
-router.get('/check', async function(req, res) {
-     var s = await student.find();
-  res.send(s);
-});
 
 module.exports = router;
