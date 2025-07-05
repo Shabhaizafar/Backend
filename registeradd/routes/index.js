7ff9390a3b17f4bb3d109fc41f0f8907cd8babea
Shabhaizafar <zafarshabhai03@gmail.com>
2025-07-05 14:33:33 +0530

    With Date

--- registeradd/routes/index.js
+++ registeradd/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var student = require('./users');
+var data = require('./users');
 
 /* GET home page. */
 // registerpage
@@ -11,42 +11,50 @@ var student = require('./users');
 
 
 // register submit
-// router.post('/submit', async function(req, res) {
-//   var obj = req.body;
-//      var s = await student.create({
-//       firstname : obj.firstname,
-//       lastname : obj.lastname
-//     });
-//   res.send("Register Successfully !!!");
-// });
+router.post('/submit', async function(req, res) {
+  var obj = req.body;
+  var d1 = new Date(obj.date);
+     var s = await data.create({
+      firstname : obj.firstname,
+      date : d1
+    });
+  console.log(s);
+  res.send("Register Successfully !!!");
+});
 
-// router.get('/check', async function(req, res) {
-//      var s = await student.find();
-//   res.send(s);
-// });
+router.get('/check', async function(req, res) {
+     var s = await student.find();
+  res.send(s);
+});
 
 
 
 // login page
+// router.get('/', function(req, res) {
+//   res.render('login');
+// });
+
+// // login page : signin
+// router.post('/signin', async function(req, res) {
+//   var obj = req.body;
+
+//   var sall = await student.find({
+//     firstname : obj.firstname,
+//     lastname : obj.lastname
+//   });
+//   console.log(sall[0]?.firstname);
+//   if(!sall[0]?.firstname){
+//     res.render('index');
+//   }else{
+//     res.send("Login SuccessFully"); 
+//   }
+// });
+
 router.get('/', function(req, res) {
-  res.render('login');
+  res.render('index');
 });
 
-// login page : signin
-router.post('/signin', async function(req, res) {
-  var obj = req.body;
-
-  var sall = await student.find({
-    firstname : obj.firstname,
-    lastname : obj.lastname
-  });
-  console.log(sall[0]?.firstname);
-  if(!sall[0]?.firstname){
-    res.render('index');
-  }else{
-    res.send("Login SuccessFully"); 
-  }
-});
-
-
+// router.post('/submit',function(req,res){
+//   res.send(req.body);
+// });
 module.exports = router;
